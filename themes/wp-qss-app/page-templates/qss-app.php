0ca35ba51e37ce7424feed4664009703025e9555
josip <josip.mucak@bornfight.com>
2024-01-09 16:34:24 +0100

    add partials

--- themes/wp-qss-app/page-templates/qss-app.php
+++ themes/wp-qss-app/page-templates/qss-app.php
@@ -1,83 +1,38 @@
 <?php
 /** Template Name: QSS App Template */
 
-
-// Authors
-use wpQssApp\api\QSSApiClient;
-
 //$auth_client = new AuthenticationClient( '' );
 //$auth_client->get_token();
 
-$token = $_COOKIE['qss_token'];
+use wpQssApp\api\AuthenticationClient;
+use wpQssApp\api\QSSApiClient;
 
+//$token = $_COOKIE['qss_token'];
+$token = '';
+
+
+if ( ! empty( $_POST['qss_login'] ) ) {
+	$email    = ! empty( $_POST['email'] ) ? sanitize_email( $_POST['email'] ) : '';
+	$password = ! empty( $_POST['password'] ) ? sanitize_text_field( $_POST['password'] ) : '';
+
+//	if ( ! empty( $email ) && ! empty( $password ) ) {
+//		$client = new AuthenticationClient( '' );
+//		$client->get_token();
+//	}
+}
 
 get_header();
 
-// AUTHORS
-//$client  = new QSSApiClient( 'authors', $token );
-//$authors = $client->find_by( array() );
+$client   = new QSSApiClient( 'users', $token );
+$qss_user = $client->get_current_user();
 
-if ( ! empty( $authors['items'] ) ) {
-	foreach ( $authors['items'] as $item ) {
-		echo $item['id'] . ' - ' . $item['first_name'] . ' ' . $item['last_name'];
-	}
+if ( empty( $qss_user ) ) {
+	get_partial( 'qss/login-form' );
+} else {
+	get_partial( 'qss/screen', array(
+		'qss_user' => $qss_user,
+	) );
 }
 
-//id = 62858
-//$new_author = $client->add_new(array(
-//	'first_name' => 'Josip',
-//	'last_name' => 'Test Last Name',
-//	'birthday' => '1995-01-09T13:40:14.064Z',
-//	'biography' => '',
-//	'gender' => 'male',
-//	'place_of_birth' => 'Zagreb'
-//));
-
-//$author = $client->find_by_id(62857);
-//$update_author = $client->update_item(62857, array(
-//	'first_name' => 'Josip',
-//	'last_name' => 'Test Last Name',
-//	'birthday' => '1995-01-09T13:40:14.064Z',
-//	'biography' => 'Biography description',
-//	'gender' => 'male',
-//	'place_of_birth' => 'New York'
-//));
-
-//$delete_author = $client->delete_item(62856);
-// END AUTHORS
-
-
-// BOOKS
-//$client = new QSSApiClient( 'books', $token );
-//$items = $client->find_by(array(
-//	'direction' => 'DESC',
-//	'page' => 1,
-//));
-//
-//if ( ! empty( $items['items'] ) ) {
-//	foreach ( $items['items'] as $item ) {
-//		echo $item['id'] . ' - ' . $item['title'] . ' ' . $item['release_date'];
-//	}
-//}
-//
-//$add_new = $client->add_new(array(
-//	'author' => array('id' =>62858 ),
-//	'title' => 'WordPress as a framework',
-//	'release_date' => '2024-01-09T14:33:14.474Z',
-//	'description' => 'How to build WordPress applications',
-//	'isbn' => '8432190321',
-//	'format' => 'A4',
-//	'number_of_pages' => 300,
-//));
-
-//$book_by_id  = $client->find_by_id( 620632 );
-//$update_book = $client->update_item( 620632, array(
-//	'isbn' => '728132190321',
-//) );
-
-//$client->delete_item( 620632 );
-
-
-// END BOOKS
 
 get_footer();
---
+++ themes/wp-qss-app/partials/qss/login-form.php
@@ -0,0 +1,13 @@
+<form action="" method="POST">
+    <fieldset>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+    </fieldset>
+
+    <fieldset>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+    </fieldset>
+
+    <input type="submit" value="Login" name="qss_login">
+</form>
