13b5e244bc7cae573f9f8b024dba37c59bfc99ce
josip <josip.mucak@bornfight.com>
2024-01-09 20:34:35 +0100

    add single movie template

---
+++ themes/wp-qss-app/single-movie.php
@@ -0,0 +1,21 @@
+<?php
+get_header();
+
+
+$movie      = get_post();
+$title_meta = get_post_meta( $movie->ID, '_movie_title', true );
+?>
+
+    <main>
+        <header>
+            <h1><?php echo ! empty( $title_meta ) ? esc_html( $title_meta ) : esc_html( $movie->post_title ); ?></h1>
+        </header>
+
+        <section>
+			<?php echo wp_kses_post( $movie->post_content ); ?>
+        </section>
+    </main>
+
+<?php
+
+get_footer();
