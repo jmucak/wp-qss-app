98e543b57c70c91316b57eb0419a2d86bc681443
josip <josip.mucak@bornfight.com>
2024-01-09 13:09:06 +0100

    add api client

---
+++ themes/wp-qss-app/app/api/BooksClient.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace wpQssApp\api;
+
+class BooksClient extends QSSApiClient {
+	const URL = 'books';
+
+	public function find_by(): array {
+		$url = self::URL . '?' . http_build_query( array(
+				'orderBy'   => 'id',
+				'direction' => 'ASC',
+				'limit'     => 12,
+				'page'      => 1,
+			) );
+
+		$response = $this->get( $url );
+
+		// DO something
+
+		return ! empty( $response['items'] ) ? $response['items'] : array();
+	}
+
+	public function add_book( array $data ): array {
+		$response = $this->post( self::URL, $data );
+
+		// DO something
+		return array();
+	}
+
+	public function find_by_id( int $id ): array {
+		$url = self::URL . '/' . $id;
+
+		$response = $this->get( $url );
+
+		// DO something
+
+		return ! empty( $response['items'] ) ? $response['items'] : array();
+	}
+
+	public function update_book( int $id, array $data ): array {
+		$url = self::URL . '/' . $id;
+
+		$response = $this->put( $url, $data );
+
+		// Do something
+
+		return array();
+	}
+
+	public function delete_book( int $id ): array {
+		$url = self::URL . '/' . $id;
+
+		$response = $this->delete( $url );
+
+		// Do something
+
+		return array();
+	}
+}
--- themes/wp-qss-app/app/api/QSSApiClient.php
+++ themes/wp-qss-app/app/api/QSSApiClient.php
@@ -3,139 +3,53 @@
 namespace wpQssApp\api;
 
 use WP_Error;
-use WP_HTTP_Requests_Response;
-use WpOrg\Requests\Exception;
 
 class QSSApiClient {
 	const BASE_URL = 'https://symfony-skeleton.q-tests.com/api/v2/';
-	const TOKEN_URL = 'token';
-	const AUTHOR_URL = 'authors';
-	const BOOK_URL = 'books';
-	const TAG_URL = 'tags';
-	const USER_URL = 'users';
 
-	private function get_headers(): array {
-		return array();
+	protected function get( string $url ): array|WP_Error {
+		return $this->api_call( $url, array(
+			'method' => 'GET',
+		) );
 	}
 
-	public function get_token(): array {
+	protected function post( string $url, array $data = array() ): array|WP_Error {
 		$args = array(
-			'sslverify'   => false,
-			'data_format' => 'body',
-			'headers'     => array(
-				'Accept'       => 'application/json',
-				'Content-Type' => 'application/json',
-			),
-			'body'        => wp_json_encode( array(
-				'email'    => 'ahsoka.tano@q.agency',
-				'password' => 'Kryze4President',
-			) ),
+			'method' => 'POST',
 		);
 
-		$response = wp_remote_post( self::BASE_URL . self::TOKEN_URL, $args );
-
-		if ( empty( $response ) || is_wp_error( $response ) ) {
-			return array();
+		if ( ! empty( $data ) ) {
+			$args['body'] = wp_json_encode( $data );
 		}
 
-		$token = json_decode( wp_remote_retrieve_body( $response ), ARRAY_A );
-
-		if ( empty( $token['token_key'] ) ) {
-			return array();
-		}
-
-		setcookie( 'qss_token', $token['token_key'] );
-		setcookie( 'qss_auth', json_encode( $token ) );
-
-		return $token;
-	}
-
-	protected function get_data( string $url ): array {
-		if ( empty( $_COOKIE['qss_token'] ) ) {
-			return array();
-		}
-
-		$args = array(
-			'sslverify' => false,
-			'headers'   => array(
-				'Authorization' => 'Bearer ' . $_COOKIE['qss_token'],
-				'Accept'        => 'application/json',
-			),
-		);
-
-		$response = wp_remote_get( $url, $args );
-
-		if ( empty( $response ) || is_wp_error( $response ) ) {
-			return array();
-		}
-
-		return json_decode( wp_remote_retrieve_body( $response ), ARRAY_A );
-	}
-
-	protected function post( string $url, array $data ): array {
-		if ( empty( $_COOKIE['qss_token'] ) ) {
-			return array();
-		}
-
-		$args = array(
-			'sslverify' => false,
-			'headers'   => array(
-				'Authorization' => 'Bearer ' . $_COOKIE['qss_token'],
-				'Accept'        => 'application/json',
-				'Content-Type'  => 'application/json',
-			),
-			'body'      => wp_json_encode( $data ),
-		);
-
-		$response = wp_remote_post( $url, $args );
-
-		if ( ( empty( $response ) || is_wp_error( $response ) ) && 200 === wp_remote_retrieve_response_code( $response ) ) {
-			return array();
-		}
-
-		return json_decode( wp_remote_retrieve_body( $response ), ARRAY_A );
+		return $this->api_call( $url, $args );
 	}
 
 	protected function delete( string $url ): array|WP_Error {
-		if ( empty( $_COOKIE['qss_token'] ) ) {
-			return array();
-		}
-
-		$args = array(
-			'sslverify' => false,
-			'method'    => 'DELETE',
-			'headers'   => array(
-				'Authorization' => 'Bearer ' . $_COOKIE['qss_token'],
-				'Accept'        => 'application/json',
-				'Content-Type'  => 'application/json',
-			),
-		);
-
-		return wp_remote_request( $url, $args );
+		return $this->api_call( $url, array(
+			'method' => 'DELETE',
+		) );
 	}
 
 	protected function put( string $url, array $data ): array|WP_Error {
-		if ( empty( $_COOKIE['qss_token'] ) ) {
-			return array();
-		}
+		return $this->api_call( $url, array(
+			'method' => 'PUT',
+			'body'   => wp_json_encode( $data ),
+		) );
+	}
 
+	private function api_call( string $url, array $data ): array|WP_Error {
 		$args = array(
 			'sslverify' => false,
-			'method'    => 'PUT',
 			'headers'   => array(
 				'Authorization' => 'Bearer ' . $_COOKIE['qss_token'],
 				'Accept'        => 'application/json',
 				'Content-Type'  => 'application/json',
 			),
-			'body'      => wp_json_encode( $data ),
 		);
 
-		$response = wp_remote_request( $url, $args );
+		$args = array_merge( $data, $args );
 
-		if ( 200 !== wp_remote_retrieve_response_code( $response ) || empty( wp_remote_retrieve_body( $response ) ) ) {
-			return $response;
-		}
-
-		return json_decode( wp_remote_retrieve_body( $response ) );
+		return wp_remote_request( self::BASE_URL . $url, $args );
 	}
 }
